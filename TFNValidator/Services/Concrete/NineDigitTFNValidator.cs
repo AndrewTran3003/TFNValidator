b63b7df60199d6dd52ff5bccb5b9c46102894843
Andrew Tran <Andrew@synercon.co>
2022-02-22 23:17:42 +1100

    Add test case for nine digit tfn number

---
+++ TFNValidator/Services/Concrete/NineDigitTFNValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFNValidator.Services.Concrete
+{
+    public class NineDigitTFNValidator
+    {
+        public bool VerifyTFNNumber(string tfnNumber)
+        {
+            return false;
+        }
+    }
+}
---
+++ TFNValidator_Test/NineDigitTFN_Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TFNValidator.Services.Concrete;
+
+namespace TFNValidator_Test
+{
+    [TestClass]
+    public class NineDigitTFN_Test
+    {
+
+        [TestMethod]
+        [DataRow("648 188 480", false)]
+        [DataRow("648 188 499", false)]
+        [DataRow("648 188 519", false)]
+        [DataRow("648 188 527", false)]
+        [DataRow("648 188 535", true)]
+        [DataRow("714 925 631", true)]
+
+        public void IsTFNValid(string input, bool output)
+        {
+            NineDigitTFNValidator validator = new();
+            Assert.AreEqual(output, validator.VerifyTFNNumber(input));
+        }
+    }
+}
