c4db0e1e2f910f095849f17ec6b56116f5ea3d65
Andrew Tran <Andrew@synercon.co>
2022-02-22 22:12:55 +1100

    Add test project

---
+++ TFNValidator_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TFNValidator_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
