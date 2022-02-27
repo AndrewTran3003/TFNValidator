a26be42eec91bc36b46b42f0528ed7e035cb5bcc
Andrew Tran <Andrew@synercon.co>
2022-02-27 21:28:07 +1100

    add material ui

--- TFNValidator_FrontEnd/App/App.js
+++ TFNValidator_FrontEnd/App/App.js
@@ -6,6 +6,7 @@ function App() {
             <div>
                 <label>Tax File Number validator</label>
                 <input type="text" />
+                <button>Submit</button>
             </div>
            
         </>
