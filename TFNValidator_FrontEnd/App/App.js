34885ea57b825e62a7a3b2646ee60f57d5e3ade7
Andrew Tran <Andrew@synercon.co>
2022-02-27 21:45:13 +1100

    Add form components

--- TFNValidator_FrontEnd/App/App.js
+++ TFNValidator_FrontEnd/App/App.js
@@ -1,14 +1,46 @@
 ﻿import React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+
 
 function App() {
     return (
-        <>
-            <div>
-                <label>Tax File Number validator</label>
-                <input type="text" />
-                <button>Submit</button>
-            </div>
+        <div>
+            <Dialog open="true">
+                <Alert severity="error">This is an error alert — check it out!</Alert>
+
+                <Alert severity="success">This is a success alert — check it out!</Alert>
+                <DialogTitle>Tax File Number Validator</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Enter Tax File Number Here
+                    </DialogContentText>
+
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="tfn-input"
+                        label="Tax File Number"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+
+                <DialogActions>
+                    <CircularProgress />
+                    <Button variant="contained">Validate</Button>
+                </DialogActions>
+
+            </Dialog>
            
-        </>
+        </div>
     );
 } export default App;
