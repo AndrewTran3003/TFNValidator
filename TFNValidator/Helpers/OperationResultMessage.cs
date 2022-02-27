9ada95ebc8daa1e486389d208c5466cec26d8a3e
Andrew Tran <Andrew@synercon.co>
2022-02-27 11:26:24 +1100

    Add operationresultmessage class and tfnservice

---
+++ TFNValidator/Helpers/OperationResultMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFNValidator.Helpers
+{
+    public enum OperationResultMessageStatus
+    {
+        Success,
+        Failure
+    }
+    public class OperationResultMessage<T> where T: class
+    {
+        public OperationResultMessageStatus Status { get; }
+        public string Message { get; }
+        public T Result { get; }
+        public OperationResultMessage(OperationResultMessageStatus status, string message,T result)
+        {
+            Status = status;
+            Message = message;
+            Result = result;
+        }
+    }
+}
---
+++ TFNValidator/Services/Abstract/ITfnService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TFNValidator.Helpers;
+
+namespace TFNValidator.Services.Abstract
+{
+    public interface ITfnService
+    {
+        public OperationResultMessage<object> ValidateTfnString(string tfnString);
+    }
+}
